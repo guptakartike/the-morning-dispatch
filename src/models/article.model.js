e19feb240da40b9f9c022b708e00249bcc98c119
Kartike Gupta <kartikegupta01@gmail.com>
2026-04-09 01:36:55 +0530

    inialized the project and set up basic auth feature

---
+++ src/models/article.model.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose")
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    unique: true,
+  },
+  summary: {
+    type: String,
+    maxLength: 200,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  isPublished: {
+    type: Boolean,
+    default: false,
+  },
+
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+
+  isBreaking: {
+    type: Boolean,
+    default: false,
+  },
+
+  views: {
+    type: Number,
+    default: 0,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const articleModel = mongoose.model("Article", articleSchema)
+
+module.exports = articleModel
