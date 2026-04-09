ff46cd1debfc8ece7ed1dae67d505d5fe1c40304
Kartike Gupta <kartikegupta01@gmail.com>
2026-04-09 12:19:09 +0530

    added category features and new category routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 
 const errorMiddleware = require("./middlewares/error.middleware")
 const authRoutes = require("./routes/auth.routes")
+const categoryRoutes = require("./routes/category.routes")
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 
 
 app.use("/api/auth", authRoutes)
+app.use("/api/categories", categoryRoutes)
 
 app.use(errorMiddleware)
 
