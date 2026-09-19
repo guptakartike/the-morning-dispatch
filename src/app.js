11d6d123860a50490c870978ebfeb9e17698ad75
Kartike Gupta <kartikegupta01@gmail.com>
2026-09-19 17:46:39 +0530

    Prepare backend for deployment

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors");
 const errorMiddleware = require("./middlewares/error.middleware");
 const articleRoutes = require("./routes/article.routes");
 
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API routes
+app.use(cors());
 app.use("/api/article", articleRoutes);
 
 app.use(errorMiddleware);
