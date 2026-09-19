655d34df8c6aa787330535721f96b8449b32bd51
Kartike Gupta <kartikegupta01@gmail.com>
2026-09-19 14:26:41 +0530

    feat: implement cron scheduler for automated news ingestion and update article query filters and routes

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const app = require("./src/app")
 const connectDB = require("./src/db/db")
-const { fetchNewsBySource } = require("./src/services/news.service");
+const { startScheduler, runIngestionJob } = require("./src/jobs/scheduler");
 require("dotenv").config()
 
 const startServer = async () => {
   try {
     await connectDB();
 
-    await fetchNewsBySource("bbc-news");
-
     app.listen(process.env.PORT, () => {
       console.log("TMD Server running");
     });
+
+    startScheduler();
+
+    await runIngestionJob();
   } catch (error) {
     console.error(error);
     process.exit(1);
--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -1,13 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
-const { getArticles, getArticleBySlug, getTrendingArticles } = require('../controllers/article.controller')
+const router = express.Router();
+const { getArticles } = require('../controllers/article.controller');
 
-//public routes
-router.get('/',getArticles)
-router.get('/trending',getTrendingArticles)
-router.get('/:slug',getArticleBySlug)
+// Read-only articles route
+router.get('/', getArticles);
 
-
-
-module.exports = router
+module.exports = router;
